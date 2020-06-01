64ee125833ba6bfcd958b5cd6f76b3f9bb944450
Alexandrew <andrew.alexandrow97@gmail.com>
2020-06-01 09:03:14 +0300

    Added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,34 @@
 // outsource dependencies
-import _ from 'lodash';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import {useDispatch, useSelector} from 'react-redux';
-import React, {memo, useState, useCallback, useMemo, useEffect} from 'react';
-import {Container} from '@material-ui/core';
+import React, {memo} from 'react';
+import Typography from "@material-ui/core/Typography";
 import {createStyles, makeStyles} from '@material-ui/core/styles';
 
 // local dependencies
-import Grid from "@material-ui/core/Grid";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import CardActions from "@material-ui/core/CardActions";
 
 
 const useStyles = makeStyles(theme =>
     createStyles({
         header: {
-            width: '100vw',
-            height: '15vh',
             display: 'flex',
-            backgroundColor: 'black',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            height: '15vh',
             marginBottom: 30,
-        }
+            backgroundColor: 'black',
+            padding: 20,
+        },
+        title: {
+            color: '#fff',
+            fontSize: '3rem',
+        },
     })
 );
 
 const Main = memo(() => {
     const classes = useStyles();
 
-    // const {data} = useSelector(selector);
-
-    // const dispatch = useDispatch();
-    // const getDataRequest = useCallback(() => dispatch({type: TYPES.GET_DATA}), [dispatch]);
-    // const setNewData = useCallback(data => dispatch({type: TYPES.DATA, data}), [dispatch]);
-    // const getDataOfCity = useCallback(id => dispatch({type: TYPES.GET_DATA_CITY, id}), [dispatch]);
-
     return <header className={classes.header}>
-
+        <Typography className={classes.title} component="h1" > Weather</Typography>
     </header>
 });
 
