8095eabe1953a8a6ee451aa9b0e5a4ef0be88081
Alexandrew <andrew.alexandrow97@gmail.com>
2020-06-04 17:04:05 +0300

    Updates

---
+++ precache-manifest.ad8a021b746d7230cda7825751878577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32366e2bb8789719fbc051d9539ab95",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "67f7f75ca7b7d1f297c3",
+    "url": "/weather/static/css/main.deb06292.chunk.css"
+  },
+  {
+    "revision": "fca1789dbaa238db561a",
+    "url": "/weather/static/js/2.e0882eb4.chunk.js"
+  },
+  {
+    "revision": "67f7f75ca7b7d1f297c3",
+    "url": "/weather/static/js/main.c13f2961.chunk.js"
+  },
+  {
+    "revision": "bcbbf36160edab700d41",
+    "url": "/weather/static/js/runtime-main.ee98893b.js"
+  }
+]);
