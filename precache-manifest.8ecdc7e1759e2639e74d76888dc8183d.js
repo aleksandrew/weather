ae3eecd4e22435245b4689bf10a60952cde26560
Alexandrew <andrew.alexandrow97@gmail.com>
2020-06-08 15:11:01 +0300

    Updates

---
+++ precache-manifest.8ecdc7e1759e2639e74d76888dc8183d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12214636c658156eeb0dabb75da0204a",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "94385abfdc326b3abfe2",
+    "url": "/weather/static/css/main.deb06292.chunk.css"
+  },
+  {
+    "revision": "bd291d20cf588ac76e20",
+    "url": "/weather/static/js/2.e64620f2.chunk.js"
+  },
+  {
+    "revision": "095637327dcea00717366af1c32b6a95",
+    "url": "/weather/static/js/2.e64620f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94385abfdc326b3abfe2",
+    "url": "/weather/static/js/main.429c6f00.chunk.js"
+  },
+  {
+    "revision": "b83e05f3b1e38480a3ef",
+    "url": "/weather/static/js/runtime-main.d16f45af.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.a3a236b7019210ab1a7650852e787793.js"
+  "/weather/precache-manifest.8ecdc7e1759e2639e74d76888dc8183d.js"
 );
 
 self.addEventListener('message', (event) => {
